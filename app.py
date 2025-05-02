f151f86b5d1f6250172b556bef62ed92816a2e93
BiancaShira <wanjirubianca97@gmail.com>
2025-05-02 14:58:02 +0300

    Added a top a tag, connected mysql

--- app.py
+++ app.py
@@ -7,18 +7,16 @@ from sqlalchemy.sql import func
 basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your secret key'
-app.config['SQLALCHEMY_DATABASE_URI']='\
-        'mysql://username:password@host:port/mydatabase' + os.path.join(basedir, 'database.db')'
+app.config['SQLALCHEMY_DATABASE_URI']='mysql://root:Pa@ssw0rd@localhost:3306/mydatabase'   + os.path.join(basedir , 'database.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATION']='False'
 
 db = SQLAlchemy(app)
 
-# ...
 
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
-    created_at = db.Column(db.DateTime(timezone=True),
+    added_at = db.Column(db.DateTime(timezone=True),
                            server_default=func.now())
     daily = db.Column(db.Text)
     weekly = db.Column(db.Text)
@@ -28,11 +26,9 @@ class Todo(db.Model):
         return f'<Todo {self.name}>'
 
 to_dos = [ ]
-@app.route("/")
-def index ():
-              return render_template("index.html", to_dos=to_dos)
 
-@app.route("/list/", methods = ("GET", "POST"))
+
+@app.route("/", methods = ("GET", "POST"))
 def list():
          if request.method == 'POST':
               title = request.form['title']
@@ -44,4 +40,8 @@ def list():
               else:
                      to_dos.append({'title': title, 'content': content})
                      return redirect(url_for('index'))
-         return render_template("to_do_list.html")
+         return render_template("index.html")
+
+@app.route("/list/")
+def index ():
+              return render_template("to_do_list.html", to_dos=to_dos)
